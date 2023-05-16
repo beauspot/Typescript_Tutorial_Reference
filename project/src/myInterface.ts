9962ce8c012e59cf6ed1ee0d548ad7b51b5a47de
beauspot <beauspot@outlook.com>
2023-05-16 20:28:10 +0100

    learnt how to extend an interface and also how to fields of an interface.

--- project/src/myInterface.ts
+++ project/src/myInterface.ts
@@ -4,22 +4,50 @@ interface User__ {
   userId: number;
   gogoleID?: string;
   // methods in an interface
-    startTrial: () => string;
-    startBasics(): string;
-    getCoupons(couponName: string, value: number): number; 
+  startTrial: () => string;
+  startBasics(): string;
+  getCoupons(couponName: string, value: number): number;
+}
+
+// extending the User__ interface with a github property
+// this feature is usually called reopening of the interface.
+interface User__ {
+  githubID: string;
+}
+
+// Inheritance with interfaces
+interface Admin extends User__ {
+  role: "admin" | "user" | "super_admin";
 }
 
 const hitesh: User__ = {
   _id: 22,
   email: "beauspot@outlook.com",
   userId: 12345,
-    startTrial: () => "Trial Started",
-    startBasics: () => {
-        return " Start basics";
-    },
-    getCoupons: (name: "Beau Spot", off: 10) => {
-        return 10
-    }
+  githubID: "beauspot",
+  startTrial: () => "Trial Started",
+  startBasics: () => {
+    return " Start basics";
+  },
+  getCoupons: (name: "Beau Spot", off: 10) => {
+    return 10;
+  },
+};
+
+// An Admin interface
+const admin_user: Admin = {
+  _id: 22,
+  email: "beauspot@outlook.com",
+  userId: 12345,
+  role: "admin",
+  githubID: "beauspot",
+  startTrial: () => "Trial Started",
+  startBasics: () => {
+    return " Start basics";
+  },
+  getCoupons: (name: "Beau Spot", off: 10) => {
+    return 10;
+  },
 };
 
 // changing values in an interface is  allowed
